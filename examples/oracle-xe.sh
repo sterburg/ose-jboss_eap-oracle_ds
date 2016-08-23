3965d715a42785b5c4a2ad85d53a3ce4a9a057eb
sterburg <sterburg@redhat.com>
2016-08-23 10:29:51 +0200

    modified oracle-xe image usage

--- examples/oracle-xe.sh
+++ examples/oracle-xe.sh
@@ -1,1 +1,5 @@
 docker run -d --name=oracle -p 49160:22 -p 1521:1521 -e ORACLE_ALLOW_REMOTE=true wnameless/oracle-xe-11g
+# oc adm policy add-scc-to-user anyuid system:serviceaccount:myproject:default
+# oc new-app --docker-image=sterburg/oracle-xe-11g --name=oracle
+# oc patch privileged:true + runAsUser:0
+# oc create -f examples/oracle-dc.yml
